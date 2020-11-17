6322e3a2a06bff9009901939a4aac3c474296fc8
Greg Gardini <ggard214@gmail.com>
2020-11-17 15:02:20 -0700

    Phase 2 complete

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", "pug");
 
 app.get('/', (req, res) =>{
     res.send("Hello from Express!")
 })
 
+app.all('*', (req, res) => {
+    res.render('generic', { method:req.method, path:req.path, randomNum:Math.round(Math.random())})
+})
 
 const port = 8081
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// A change
+
